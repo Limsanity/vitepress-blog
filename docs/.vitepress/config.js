2c99f56bf59f8ea68f05f74b2b78e731c51554ad
钟霖 <zhonglin01@corp.netease.com>
2021-02-24 16:53:00 +0800

    feat(config): config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,26 @@
+const path = require("path");
+const fs = require("fs");
+
+const readFilesFromDir = (path) => {
+  const files = fs.readdirSync(path);
+  return files
+    .filter((file) => file !== "index.md")
+    .map((file) => file.slice(0, -3));
+};
+
+const getSidebars = (dir) => {
+  const dirPath = path.join(__dirname, "..", dir);
+  const files = readFilesFromDir(dirPath);
+  return files.map((file) => ({
+    text: file,
+    link: `/${dir}/${file}.html`,
+  }));
+};
+
 module.exports = {
   title: "Lim's Blog",
+  themeConfig: {
+    nav: [{ text: "首页", link: "/" }],
+    sidebar: [{ text: "Web", link: "/Web/", children: getSidebars("Web") }],
+  },
 };
